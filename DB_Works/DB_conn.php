757bc4a8c34c8d2e83e4a76cd2c0fcbf3228748d
Diwakar Kumar <dwkr16@gmail.com>
2016-06-09 22:12:28 +0530

    Geek Blog on PHP
    PHP PDO Operations.

---
+++ DB_Works/DB_conn.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Defining Data Source Name
+ * Username and Password
+ * of your Database 
+ */
+$dsn='mysql:host=localhost;dbname=geek_blog';
+$username='root';
+$password='tiger';
+
+ try
+  {
+	$conn=new PDO($dsn,$username,$password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo 'connected';
+  }
+ catch(PDOException $e)
+     
+  {
+	echo $e->getMessage();
+  }
+
+
+?>
